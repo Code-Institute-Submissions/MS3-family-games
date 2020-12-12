78d4e8ea125ed279e84358687a8f3ce4f2738323
miss-monkey <justina278@gmail.com>
2020-12-12 19:26:47 +0000

    Add icons to Game cards and navbar

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import (
     Flask, flash, render_template,
-    redirect, request, session, url_for)
+    redirect, request, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
