b1a1afcd98e310fdb242687f5652c72225591086
miss-monkey <justina278@gmail.com>
2020-12-12 14:58:21 +0000

    Create Edit button functionality

--- app.py
+++ app.py
@@ -45,10 +45,30 @@ def add_game():
         }
         mongo.db.games.insert_one(game)
         flash("Game Successfully Added")
-        return redirect(url_for("home"))
+        return redirect(url_for("get_games"))
 
     games = list(mongo.db.games.find())
     return render_template("add_game.html", games=games)
+
+
+@app.route("/edit_game/<game_id>", methods=["GET", "POST"])
+def edit_game(game_id):
+    if request.method == "POST":
+        submit = {
+            "category": request.form.get("category"),
+            "game_name": request.form.get("game_name"),
+            "age_range": request.form.get("age_range"),
+            "number_of_players": request.form.get("number_of_players"),
+            "link_to_shop": request.form.get("link_to_shop"),
+            "image_url": request.form.get("image_url"),
+            "description": request.form.get("description")
+        }
+        mongo.db.games.update({"_id": ObjectId(game_id)}, submit)
+        flash("Game Successfully Updated")
+        return redirect(url_for("get_games"))
+
+    game = mongo.db.games.find_one({"_id": ObjectId(game_id)})
+    return render_template("edit_game.html", game=game)
 
 
 if __name__ == "__main__":
